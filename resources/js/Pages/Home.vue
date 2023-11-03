0828a333391e890171a53df28962fd852fd652fe
nyattonguyen <nyattonguyen@gmail.com>
2023-11-03 22:03:45 +0700

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+    <Head title="Home" />
+    <MainLayout>
+        <div class="bg-red-500">HOME NE</div>
+    </MainLayout>
+</template>
